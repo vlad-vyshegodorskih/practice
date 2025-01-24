433d7b4656077e5ec5513ae21b40b516f96ab4d0
Dislav <v.vyshegorodskih@gmail.com>
2025-01-24 09:40:24 +0000

    End of lesson

---
+++ script_other.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('Я сыт!');
+// }
+
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0
+
+// console.log(hamburger === 3 && cola && fries);
+
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'just a string');
+
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Все сыты!');
+// } else {
+//     console.log('Мы уходим');
+// }
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Все довольны!');
+// } else {
+//     console.log('Мы уходим');
+// }
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+// console.log(!0);
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// console.log(hamburger || cola || fries === 3 || nuggets);
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+// console.log(hamburger && cola || fries === 3 && nuggets);
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+
+
+
+
+
+
