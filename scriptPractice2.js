84bd25ada6bc6d8502f2bb23fe4468447a752ce1
Dislav <v.vyshegorodskih@gmail.com>
2025-01-25 09:51:13 +0000

    Next

--- scriptPractice2.js
+++ scriptPractice2.js
@@ -24,7 +24,8 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-}
+};
+
 
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
@@ -38,17 +39,16 @@ for (let i = 0; i < 2; i++) {
         i--;
     }
     
-    personalMovieDB.movies[a] = b;
-}
+};
 
-if (personalMovieDB.count <= 10) {
-    console.log("Просмотрено довольно мало фильмов!");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель!");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман!");
-} else {
-    console.log("Произошла ошибка!");
-}
+// if (personalMovieDB.count <= 10) {
+//     console.log("Просмотрено довольно мало фильмов!");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("Вы классический зритель!");
+// } else if (personalMovieDB.count >= 30) {
+//     console.log("Вы киноман!");
+// } else {
+//     console.log("Произошла ошибка!");
+// }
 
 console.log(personalMovieDB);
