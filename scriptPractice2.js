f112e882d8d746d265467b0e658b9d2f23496c22
Dislav <v.vyshegorodskih@gmail.com>
2025-01-25 09:05:14 +0000

    Continue lesson

--- scriptPractice2.js
+++ scriptPractice2.js
@@ -15,3 +15,44 @@
 'use strict';
 
 // Код возьмите из предыдущего домашнего задания
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.lenght < 50) {
+        personalMovieDB.movies[a] = b;    
+        console.log('Done!');
+    } else {
+        console.log('Error!');
+        i--;
+    }
+    
+    personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count <= 10) {
+    console.log("Просмотрено довольно мало фильмов!");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель!");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман!");
+} else {
+    console.log("Произошла ошибка!");
+}
+
+console.log(personalMovieDB);
