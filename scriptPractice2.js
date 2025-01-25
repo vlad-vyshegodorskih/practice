9929794092ed823a0426c11fbf9454df1d835009
Dislav <v.vyshegorodskih@gmail.com>
2025-01-25 09:17:23 +0000

    Code error. Start debugging

--- scriptPractice2.js
+++ scriptPractice2.js
@@ -26,10 +26,6 @@ const personalMovieDB = {
     privat: false
 }
 
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
