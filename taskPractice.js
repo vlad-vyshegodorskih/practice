dca6fdf4f8be2e9ed09629453bae3e6c82ff1baa
Dislav <v.vyshegorodskih@gmail.com>
2025-02-12 15:46:04 +0000

    Methods and properites practice end

--- taskPractice.js
+++ taskPractice.js
@@ -1,6 +1,34 @@
 'use strict';
 
-const str = "test";
-const arr = [1, 2, 4]
 
-console.log(arr.length);
+//STRING METHODS
+const str = "teSt";
+
+
+console.log(str.toUpperCase());  //возводит в верхний регистр
+console.log(str.toLowerCase());  //возводит в нижний регистр
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));// выводит индекс укзазанного символа строки
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11)); // отрезает часть строки по указанному диапазону, может работать с отрицательными значениями
+
+console.log(logg.substring(6, 11));// тоже что и silce но не поддерживает отрицательные значения
+
+console.log(logg.substr(6, 5));// как и предыдущие, только второй аргумент указывает количество вырезаемых символов
+
+/////
+
+
+//NUMBER METHODS
+
+const num = 12.2;
+console.log(Math.round(num));  //округляет чисто до ближайщего целого
+
+const test = "12.2px";
+console.log(parseInt(test)); // переводит тип данных в number отрезая все кроме целого числа, не округляет.
+console.log(parseFloat(test)); //переводит тип данных в number сохраняя дробное значение, не округляет.
