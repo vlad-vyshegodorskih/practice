66f8261fad16d0709147d6fe348e5b04d769c91d
Dislav <v.vyshegorodskih@gmail.com>
2025-02-14 13:31:28 +0000

    OOP LESSON START

--- script_other.js
+++ script_other.js
@@ -1,5 +1,40 @@
 'use strict';
 
+// const personalPlanPeter = {
+//     name: "Peter",
+//     age: "29",
+//     skills: {
+//         languages: ['ru', 'eng'],
+//         programmingLangs: {
+//             js: '20%',
+//             php: '10%'
+//         },
+//         exp: '1 month'
+//     }
+// };
+
+// function showExperience(plan) {
+//     const {exp} = plan.skills;
+//     return exp;
+// }
+
+// showExperience(personalPlanPeter);
+// console.log(showExperience(personalPlanPeter));
+
+// function showProgrammingLangs(plan) {
+//     let str = "";
+//     const {programmingLangs} = plan.skills;
+
+//     for (let key in programmingLangs) {
+//         str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
+//     }
+    
+//     return str;
+// }
+
+// showProgrammingLangs(personalPlanPeter);
+// console.log(showProgrammingLangs(personalPlanPeter));
+
 
 
 
