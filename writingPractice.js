0d2096c28b0adbec6520c53cebdf7d2d10a9f5ec
Dislav <v.vyshegorodskih@gmail.com>
2025-02-16 11:20:57 +0000

    Continue repeating with logical operators

--- writingPractice.js
+++ writingPractice.js
@@ -1,1 +1,37 @@
 'use strict';
+
+if (4 == 9) {
+    console.log("Ok!");
+} else {
+    console.log("Error!");
+}
+
+
+
+if (num < 49) {
+    console.log("error");
+} else if (num > 100) {
+    console.log("Много");
+}  else {
+    console.log("Ok!");
+}
+
+(num === 50) ? console.log("Ok!") : console.log("error");
+
+
+const num = 50;
+
+switch (num) {
+    case 49: 
+        console.log("error");
+        break;
+    case 100: 
+        console.log("error");
+        break;    
+    case 50: 
+        console.log("OK");
+        break;
+    default:
+        console.log("нет верного ответа");
+        break;
+}
