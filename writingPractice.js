de91cdbe346a1a659501daac498bf1d651635f00
Dislav <v.vyshegorodskih@gmail.com>
2025-02-25 17:15:15 +0000

    Function lesson done

--- writingPractice.js
+++ writingPractice.js
@@ -121,3 +121,58 @@
 
 
 //FUNCTIONS
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+  
+}
+
+showFirstMessage("Hello Wordl!");
+console.log(num);
+
+
+function calc(a, b) {
+    return (a + b);
+}
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+const usdCurr = 28;
+const discount = 0.9;
+
+
+function convert (amount, curr) {
+    return curr * amount;
+}
+
+function promotion(result) {
+    console.log(result * discount);
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return
+    }
+    console.log("Done");
+}
+
+test();
+
+function doNothing() {};
+console.log(doNothing() === undefined);
